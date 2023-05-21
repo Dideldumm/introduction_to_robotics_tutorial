ffa34bb7bce774506696e83274263c9100e2cd69
Janik Teune <janik.teune@ovgu.de>
2023-05-21 20:41:31 +0200

    Zwischenstand

--- src/reactive_behaviour/reactive_behaviour/controller.py
+++ src/reactive_behaviour/reactive_behaviour/controller.py
@@ -5,41 +5,54 @@ from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 from std_msgs.msg import String
 
-class VelocityController(Node):
 
+class VelocityController(Node):
     def __init__(self):
-        super().__init__('velocity_controller')
-        self.publisher = self.create_publisher(Twist, 'cmd_vel', 10)
-        self.forward_distance = 0
-        self.create_subscription(LaserScan, 'scan', self.laser_cb, rclpy.qos.qos_profile_sensor_data)
-        self.create_subscription(String, 'status', self.score_status_cb, 10)
+        super().__init__("velocity_controller")
+        self.publisher = self.create_publisher(Twist, "cmd_vel", 10)
+        self.create_subscription(
+            LaserScan, "scan", self.laser_cb, rclpy.qos.qos_profile_sensor_data
+        )
+        self.create_subscription(String, "status", self.score_status_cb, 10)
         self.create_timer(0.1, self.timer_cb)
-        self.get_logger().info('controller node started')
-        self.score_status = -1
-        
+        self.get_logger().info("controller node started")
+        self.active = True
+        self.score_status = 0
+        self.scan_time = 0.0
+        self.forward_distance = 0
+
     def timer_cb(self):
         msg = Twist()
-        x = self.forward_distance - 0.3
-        # if self.score_status > 15:
-        #     msg.linear.x = 0.0
-        #     msg.angular.x = 0.0
-        # if x < 0.1:
-        #     msg.angular.x = 0.7
-        #     msg.linear.x = 0.0
-        # else:
-        #     msg.linear.x = 0.2
-        #     msg.angular.x = 0.0
-        msg.linear.x = 0.2
-        
+        x = self.forward_distance - 0.4
+        if self.score_status > 15:
+            msg.linear.x = 0.0
+            msg.angular.z = 0.0
+        elif x < 0.0:
+            msg.angular.z = 0.7
+            msg.linear.x = 0.0
+        else:
+            msg.linear.x = 0.2
+            msg.angular.z = 0.0
+        if self.active:
+            self.get_logger().info(
+                "Score status: "
+                + str(self.score_status)
+                + " - Distance: "
+                + str(self.forward_distance)
+                + " - Scan time: "
+                + str(self.scan_time)
+            )
+
         self.publisher.publish(msg)
-    
+
     def laser_cb(self, msg):
         if self.score_status % 10 == 0:
-            self.get_logger().info(self.forward_distance)
+            self.get_logger().info(str(self.forward_distance))
+        self.scan_time = msg.scan_time
         self.forward_distance = msg.ranges[0]
 
     def score_status_cb(self, msg):
-        self.score_status = msg
+        self.score_status = int(msg.data) if msg.data.isdigit() else self.score_status
 
 
 def main(args=None):
@@ -55,5 +68,6 @@ def main(args=None):
     node.destroy_node()
     rclpy.shutdown()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
